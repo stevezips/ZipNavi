7f34385dd0c4ee8425c3547432c1dcfbbd47e97f
shkangr <rkdtmdgjs@naver.com>
2019-09-25 23:34:26 +0900

    Update app.js

--- src/tet/app.js
+++ src/tet/app.js
@@ -1,8 +1,5 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,10 +9,21 @@ var crawlNews = require('./utils/CrawlNews');
 
 var app = express();
 
+var whitelist = ['*'];
+/*var corsOptions = {
+                origin: function(origin, callback){
+                        var isWhitelisted = whitelist.indexOf(origin) !== -1;
+                        callback(null, isWhitelisted);
+                },
+        credentials:true
+}*/
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
